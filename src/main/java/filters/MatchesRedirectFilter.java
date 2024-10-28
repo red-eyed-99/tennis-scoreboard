0ad6e67a9505fafed2aae67428a261f54a64f557
red-eyed-99 <mgnblack99@gmail.com>
2024-10-28 20:41:13 +0300

    refactor: move redirect logic to separate filter class

---
+++ src/main/java/filters/MatchesRedirectFilter.java
@@ -0,0 +1,41 @@
+package filters;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.SneakyThrows;
+
+@WebFilter(urlPatterns = {"/matches", "/matches*"})
+public class MatchesRedirectFilter implements Filter {
+
+    @Override
+    @SneakyThrows
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) {
+        var request = (HttpServletRequest) servletRequest;
+        var response = (HttpServletResponse) servletResponse;
+
+        var page = request.getParameter("page");
+        var filterByPlayerName = request.getParameter("filter_by_player_name");
+
+        if (page == null && filterByPlayerName == null) {
+            response.sendRedirect("/matches?page=1");
+            return;
+        }
+
+        if (page == null) {
+            response.sendRedirect("/matches?page=1&filter_by_player_name=" + filterByPlayerName);
+            return;
+        }
+
+        if (filterByPlayerName != null && filterByPlayerName.isBlank()) {
+            response.sendRedirect("/matches?page=" + page);
+            return;
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
--- src/main/java/servlets/MatchesServlet.java
+++ src/main/java/servlets/MatchesServlet.java
@@ -10,30 +10,17 @@ import lombok.SneakyThrows;
 import models.entities.Match;
 import services.FinishedMatchesService;
 import utils.HibernateUtil;
-import utils.MatchesPaginator;
+import paginator.MatchesPaginator;
 import utils.ResponseErrorSender;
 
-import java.io.IOException;
 import java.util.List;
 
 @WebServlet(urlPatterns = {"/matches", "/matches*"})
 public class MatchesServlet extends HttpServlet {
 
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        var page = request.getParameter("page");
-        var filterByPlayerName = request.getParameter("filter_by_player_name");
-
-        if (page == null && filterByPlayerName == null) {
-            response.sendRedirect("/matches?page=1");
-            return;
-        }
-
-        if (page == null) {
-            response.sendRedirect("/matches?page=1&filter_by_player_name=" + filterByPlayerName);
-            return;
-        }
-
+    @SneakyThrows
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) {
         try {
             showMatches(request, response);
         } catch (NumberFormatException | PageNotFoundException ex) {
